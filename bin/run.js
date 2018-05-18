ef9c5f327fd71ec8d99d1cf3a3a944315060d027
marcoguilherme <mgbm13@gmail.com>
2018-05-17 21:59:20 -0300

    Alterada a versao do plugin do slack e configurado para escutar mensagens

--- bin/run.js
+++ bin/run.js
@@ -6,11 +6,13 @@ const http = require('http');
 
 const server = http.createServer(service);
 
-const slackToken = 'your token here';
-const rtm = slackClient.init(slackToken);
+const slackToken = 'xoxb-366849263350-366871716038-uprYOtftrcBTkXM9rM6U1KCL';
+const slackLogLevel = 'verbose';
+
+const rtm = slackClient.init(slackToken, slackLogLevel);
 rtm.start();
 
-server.listen(3000);
+slackClient.addAuthenticatedHandler(rtm, ()=>{ server.listen(3000); })
 
 server.on('listening', function() {
     console.log(`IRIS is listening on ${server.address().port} in ${service.get('env')} mode.`);
--- server/slackClient.js
+++ server/slackClient.js
@@ -1,16 +1,31 @@
-const { RTMClient } = require('@slack/client');
+const RtmClient = require('@slack/client').RtmClient;
+const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+let rtm = null;
 
 function handleOnAuthenticated(rtmStartData){
     console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to the channel`)
 }
 
-function addAuthenticatedHandler(rtm, handler){
-    rtm.on('authenticated', handler);
+function handleOnMessage(message) {
+    console.log(message);
+
+  rtm.sendMessage('this is a test message', message.channel, function messageSent() {
+    // optionally, you can supply a callback to execute once the message has been sent
+  });
 }
 
-module.exports.init = function slackClient(token){
+
+function addAuthenticatedHandler(rtm, handler){
+    rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, handler);
+}
+
+module.exports.init = function slackClient(token, logLevel){
     // The client is initialized and then started to get an active connection to the platform
-    const rtm = new RTMClient(token);
+    rtm = new RtmClient(token, {logLevel: logLevel});
     addAuthenticatedHandler(rtm, handleOnAuthenticated)
+    rtm.on(RTM_EVENTS.MESSAGE, handleOnMessage);
     return rtm;
 }
+
+module.exports.addAuthenticatedHandler = addAuthenticatedHandler;
